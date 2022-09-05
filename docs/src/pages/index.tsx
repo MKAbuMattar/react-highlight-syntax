16b6e8c71a08a74d7edccc83f2363fb5bb1ae284
MKAbuMattar <mohammad.khaled@outlook.com>
2022-09-05 21:06:59 +0300

    ci: add husky | commitlint

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next';
 
-import ReactHighlightSyntax from 'react-highlight-syntax';
+import ReactHighlightSyntax, {Language} from 'react-highlight-syntax';
 
-const code = `const foo = 'bar';`;
+const code = `const foo: string = 'bar';
+const baz: number = 42;
+const qux: boolean = true;
+const quux: null = null;
+const corge: undefined = undefined;
+const grault: symbol = Symbol('grault');
+const garply: bigint = 123n;
+const waldo: object = { waldo: true };
+const fred: any = 'fred';
+const plugh: unknown = 'plugh';
+const xyzzy: never = undefined;
+const thud: void = undefined;
+`;
 
 const Home: NextPage = () => {
   return (
     <>
       <ReactHighlightSyntax
-        language={'JavaScript'}
-        theme={'Base16Darcula'}
+        language={'TypeScript'}
+        theme={'NnfxDark'}
         copy={true}
         copyBtnTheme={'Dark'}
       >
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import ReactHighlightSyntax from './components/Highlight';
+import { Language, Theme } from './types/Highlight';
+import { CopyBtnTheme } from './types/Copy';
 
 export * from './components/Highlight';
+export * from './types/Highlight';
+export * from './types/Copy';
 
+export { Language, Theme };
+export { CopyBtnTheme };
 export default ReactHighlightSyntax;
