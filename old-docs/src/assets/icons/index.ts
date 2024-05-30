a0ac3ced9c363f1151d6dfaf0880c089be1783ee
MKAbuMattar <mohammad.khaled@outlook.com>
2024-05-30 19:03:07 +0300

    chore: Remove unused configuration files and scripts

--- old-docs/src/assets/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MenuIcon } from './menu';
-export { default as APIIcon } from './api';
-export { default as PackageIcon } from './package';
-export { default as CodeIcon } from './code';
